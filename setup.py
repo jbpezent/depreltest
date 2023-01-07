dfbef8c5c3c371dab74f9414dc87d4cbec8c2d7f
jbpezent <jbpezent@crimson.ua.edu>
2023-01-07 17:20:04 -0600

    Again

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ class CMakeBuild(build_ext):
             "-DPYTHON_EXECUTABLE=" + sys.executable,
             "-DPIP_INSTALL=True",
         ]
-        build_args = ["--target", "_depreltest", "sphinx"]
+        build_args = ["--target", "_depreltest"]
 
         cfg = "Debug" if self.debug else "Release"
         build_args += ["--config", cfg]
